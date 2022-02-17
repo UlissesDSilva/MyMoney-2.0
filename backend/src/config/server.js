5ed2ad24e8f66af9b3e119303dbd94c5337895d7
Ulisses Queiroz D. Silva <uqs.dsilva@gmail.com>
2022-02-17 20:59:49 -0300

    Tratamento de erros e middleware para as requisições

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser')
+const queryParse = require('express-query-int')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 const port = 3003;
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParse())
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}`)
